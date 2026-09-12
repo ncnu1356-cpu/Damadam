c1074d4fbb1680f42d9f8460c18f51116c6e9fa7
ncnu1356-cpu <ncnu1356@gmail.com>
2026-09-12 16:41:04 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
